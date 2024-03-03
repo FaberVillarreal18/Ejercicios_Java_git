0ee4da91103e5dff67036b1941fabcabdaf01b7a
Faber Villarreal <faber.villarrealantequer@unicolombo.edu.co>
2024-03-03 17:43:59 -0500

    facilito

--- 1-20/Ejercicio14.java
+++ 1-20/Ejercicio14.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Ejercicio14 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        System.out.println("Ingresa un numero entero: ");
+        System.out.println("Ingresa un numero entero de 4 digitos: ");
         int n1=entrada.nextInt();
-        
+        int a=n1/1000;
+        int b=(n1/100)%10;
+        int c=(n1/10)%10;
+        int d=n1%10;
+        int op=a+b+c+d;
+        System.out.println("Los digitos son: "+a+", "+b+", "+c+", "+d);
+        System.out.println("La suma de los digitos es: "+op);
+        entrada.close();
     }
 }
