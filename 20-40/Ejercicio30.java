44a74c43d29404b02e1d4f92001ab88f42d090af
Faber Villarreal <faber.villarrealantequer@unicolombo.edu.co>
2024-03-09 21:08:46 -0500

    me faltaba cerrar el scanner

--- 20-40/Ejercicio30.java
+++ 20-40/Ejercicio30.java
@@ -6,5 +6,6 @@ public class Ejercicio30 {
         double n=entrada.nextDouble();
         double op=n*1000;
         System.out.println("La distancia en metros es de: "+op+"m");
+        entrada.close();
     }
 }
