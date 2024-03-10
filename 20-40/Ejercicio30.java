da92b80f1e9a766e48240cd0e19066b4e03b5b5e
Faber Villarreal <faber.villarrealantequer@unicolombo.edu.co>
2024-03-09 21:07:20 -0500

    Ejercicio 30

---
+++ 20-40/Ejercicio30.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Ejercicio30 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingresa la distancia en kilometros: ");
+        double n=entrada.nextDouble();
+        double op=n*1000;
+        System.out.println("La distancia en metros es de: "+op+"m");
+    }
+}
