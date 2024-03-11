6f9af375f27f3032944583f4df24197222ace4f6
Faber Villarreal <faber.villarrealantequer@unicolombo.edu.co>
2024-03-11 16:07:26 -0500

    Ejercicio 39

---
+++ 21-40/Ejercicio39.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio39 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingresa el año en que naciste: ");
+        int n=entrada.nextInt();
+        System.out.println("Ingresa tu nombre: ");
+        String x=entrada.next();
+        int op=2024-n;
+        double op2=365*0.34;
+        double op3=(int)op2*op;
+        System.out.println("Su nomber es: "+x );
+        System.out.println("Ha dormido "+op3+" dias");
+        entrada.close();
+    }
+}
