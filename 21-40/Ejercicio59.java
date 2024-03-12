490be0a9dbd6ed770fe90452bf40e7f01d4f7d06
Faber Villarreal <faber.villarrealantequer@unicolombo.edu.co>
2024-03-12 14:11:55 -0500

    Ejercicio 59

---
+++ 21-40/Ejercicio59.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio59 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número del 1 al 10:");
+        int n = scanner.nextInt();
+
+        if (n >= 1 && n <= 10) {
+            System.out.println("Tabla de multiplicar de " + n + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(n + " x " + i + " = " + (n * i));
+            }
+        } else {
+            System.out.println("El número ingresado está fuera del rango válido.");
+        }
+
+        scanner.close();
+    }
+}
+
